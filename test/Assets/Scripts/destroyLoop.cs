19a5d4864041cfa933ec768f84aabd48c4237e18
AidenBow <AidenBow@users.noreply.github.com>
2020-01-22 19:31:13 -0800

    destroy is on invoke loop

--- test/Assets/Scripts/destroyLoop.cs
+++ test/Assets/Scripts/destroyLoop.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class destroyLoop : MonoBehaviour
 {
     public GameObject[] objects;
+    int counter = 0;
     void Start()
     {
-        foreach (GameObject o in objects)
-        {
-            print("destroyed! " + o);
-            Destroy(o);
-        }
+        //foreach (GameObject o in objects)
+        //{
+            //o.GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
+            Invoke("Kill", 1);
+            objects[counter].GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
+        //print("destroyed! " + o);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Kill()
+    {
+        
+        Destroy(objects[counter]);
+        counter = counter + 1;
+        objects[counter].GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
+        print(counter);
+        Invoke("Kill", 1);
     }
 }
