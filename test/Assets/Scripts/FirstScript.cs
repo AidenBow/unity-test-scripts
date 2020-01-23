c6ed4f2623119bf558c8160f1615e5dc9c2b372c
AidenBow <AidenBow@users.noreply.github.com>
2020-01-22 18:19:39 -0800

    unity project added

---
+++ test/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstScript : MonoBehaviour
+{
+
+    public float speed = 0.01f;
+    public Color[] colors = new Color[] {new Color(0, 0, 1, 1), new Color(0, 1, 1, 1), new Color(1, 0, 1, 1)};
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("console is running");
+        colors[0] = new Color(255, 0, 0, 1);
+    }
+
+    // Update is called once per FRAME 
+    void Update()
+    {
+            transform.Translate(speed, 0, 0);
+            ChangeColor();
+            StartCoroutine("wait");
+    }
+
+    void ChangeColor() {
+        if (counter < 2){
+            counter = counter + 1;
+        } else {
+            counter = 0;
+        };
+        
+        gameObject.GetComponent<Renderer>().material.color = colors[counter];
+       
+
+        
+    }
+
+    IEnumerator wait(){
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ test/Assets/Scripts/destroyLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyLoop : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+        foreach (GameObject o in objects)
+        {
+            print("destroyed! " + o);
+            Destroy(o);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
