2714cbe82ecd079653afccec366bb2ab37ed49db
AidenBow <AidenBow@users.noreply.github.com>
2020-01-24 21:10:36 -0800

    find script

---
+++ test/Assets/Scripts/activatorLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activatorLoop : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+        foreach(GameObject o in objects)
+        {
+            o.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ test/Assets/Scripts/findScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findScript : MonoBehaviour
+{
+    GameObject foundObj;
+    void Start()
+    {
+        foundObj = GameObject.Find("Capsule");
+        Destroy(foundObj);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
